6d9240a89835ca6197c0f0fec0e6712f30af5695
kanataaraki2601 <kanataaraki2601@gmail.com>
2026-08-21 19:08:56 +0900

    Add basic study session data

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+
+print("Study Tracker is alive.")
+print("I am learning Git.")
+
+for i in range(10):
+    print(i)
+
+subject = "Python"
+minutes = 30
+
+print("Subject:", subject)
+print("Minutes:", minutes)
+
 print("Study Tracker is alive.")
 print("Today I learned Git.")
 
